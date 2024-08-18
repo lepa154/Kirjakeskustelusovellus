706011b323c897133149bacddcd77135b966bb18
lepa154 <163763366+lepa154@users.noreply.github.com>
2024-08-18 22:26:13 +0300

    Update schema.sql

--- tables.sql
+++ tables.sql
@@ -1,24 +1,31 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
-    username TEXT, 
-    password TEXT
+    username TEXT UNIQUE, 
+    password TEXT,
 );
 
-CREATE TABLE genre (
-    id SERIAL PRIMARY KEY,
-    name TEXT
-);
-
-CREATE TABLE book (
+CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    genre_id INTEGER REFERENCES genre
+    author TEXT,
+    genre TEXT
 );
 
-CREATE TABLE review (
+CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
     content TEXT,
-    grade INTEGER
-    movie_id INTEGER
+    grade INTEGER,
+    book_id INTEGER REFERENCES books(id),
+    user_id INTEGER REFERENCES users(id),
+    sent_at TIMESTAMP
 );
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER REFERENCES users(id),
+    review_id INTEGER REFERENCES reviews(id),
+    sent_at TIMESTAMP
+);
+    
     
